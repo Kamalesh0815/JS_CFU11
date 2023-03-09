3c6192ce72d8bb0fc4bfc1f2905a3d8f8a39a147
Kamalesh0815 <118426240+Kamalesh0815@users.noreply.github.com>
2023-03-09 23:31:59 +0530

    completed

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -9,30 +9,41 @@ document.getElementById("length").addEventListener("change", charLength);
 
 function generatePass() {
   let length = charLength();
-  let upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  // let upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
-  let lowerCase = "mnbvcxzlkjhgfdssapoiuytrewq";
+  // let lowerCase = "mnbvcxzlkjhgfdssapoiuytrewq";
 
-  let number = ["0", "1", "2", "9", "3", "8", "4", "7", "5", "6"];
+  // let number = ["0", "1", "2", "9", "3", "8", "4", "7", "5", "6"];
 
-  let symbols = ["=", "?", "(", ")", "*", "&", "^", "%"];
+  let symbolsStr = `!@#$%^&*()_+{}|":?></`;
+  let upperCaseStr = `QWERTYUIOPASDFGHJKLZXCVBNM`;
+  let lowerCaseStr = `mnbvcxzlkjhgfdsapoiuytrewq`;
+  let numberStr = `1234567890`;
   let types = JSON.parse(localStorage.getItem("types"));
   let findedType = "";
+  // console.log(types);
+  let upperCase = types.find((T) => T == "upperCase");
+  let lowerCase = types.find((T) => T == "lowerCase");
+  let number = types.find((T) => T == "numbers");
+  let symbols = types.find((T) => T == "symbols");
+
+  console.log(upperCase, lowerCase, number, symbols);
+
   // for (let i = 0; i < types.length; i++) {
   //   findedType += "" + types[i];
   // }
   // let data = "";
-  // if (findedType == "uppercase") {
+  // if (findedType == "upperCase") {
   //   data = upperCase;
   // }
-  // if (findedType == "uppercaselowercase") {
+  // if (findedType == "upperCaselowerCase") {
   //   for (let i = 0; i < 26; i++) {
   //     console.log(lowerCase[i]);
   //     data += upperCase[i] + lowerCase[i];
   //   }
   // }
 
-  // if (findedType == "uppercaselowercasenumber") {
+  // if (findedType == "upperCaselowerCasenumber") {
   //   for (let i = 0; i < 26; i++) {
   //     console.log(lowerCase[i]);
   //     data += upperCase[i] + lowerCase[i];
@@ -42,7 +53,7 @@ function generatePass() {
   //   }
   // }
 
-  // if (findedType == "uppercaselowercasenumbersymbols") {
+  // if (findedType == "upperCaselowerCasenumbersymbols") {
   //   for (let i = 0; i < 26; i++) {
   //     console.log(lowerCase[i]);
   //     data += upperCase[i] + lowerCase[i];
@@ -55,76 +66,99 @@ function generatePass() {
   //   }
   // }
 
-  if ((types.length = 1)) {
-    data = types;
-  }
+  // console.log(findedType);
 
-  if (types.length == 2) {
-    for (let i = 0; i < 26; i++) {
-      if (types[0].length >= i) {
-        data += types[0][i];
-      }
-      if (types[1][i] > i) {
-        data += types[1][i];
-      }
-    }
-  }
-
-  if (types.length == 3) {
-    for (let i = 0; i < 26; i++) {
-      if (types[0].length >= i) {
-        data += types[0][i];
-      }
-      if (types[1][i] > i) {
-        data += types[1][i];
-      }
-      if (types[2][i] > i) {
-        data += types[2][i];
-      }
-    }
-  }
-
-  if (types.length == 4) {
-    for (let i = 0; i < 26; i++) {
-      if (types[0].length >= i) {
-        data += types[0][i];
-      }
-      if (types[1][i] > i) {
-        data += types[1][i];
-      }
-      if (types[2][i] > i) {
-        data += types[1][i];
-      }
-      if (types[3][i] > i) {
-        data += types[3][i];
-      }
-    }
-  }
-
-  console.log(findedType);
-
-  let password;
+  let data = `Eh_J|D<Z4}:szb"rW{c)&!6#oQkftmy@S*3L?MX/%wxia0vHPK+YUjIp2$dO8G917nluFRVs5A>eBq(CgN^T"`;
+  let password = "";
   let start = 0;
+
+  let prePass = "";
+  prePass += localStorage.getItem("password");
 
   if (localStorage.getItem("start") != null) {
     start = JSON.parse(localStorage.getItem("start"));
   }
-  start = Math.floor(Math.random() * 6) + 1;
-  console.log(start);
-  console.log(length);
+  start = Math.floor(Math.random() * 10) + 1;
+  // console.log(start);
+  // console.log(length);
   let final = 0;
   final = start + final;
-  console.log(final);
-  if (final > "26") {
+  // console.log(final);
+  if (final > "80") {
     start = 0;
   }
 
-  for (let i = 0; i < length; i++) {
-    if (password != undefined) {
-      password += data[start];
-    } else {
-      password = data[start];
+  for (let i = 0; password.length < length; i++) {
+    console.log(data[start]);
+    let currentData = data[start];
+    if (password == undefined) {
+      console.log("password");
+      password += currentData;
+      console.log(password);
     }
+
+    if (upperCase !== undefined) {
+      for (let j = 0; j < upperCaseStr.length; j++) {
+        if (currentData == upperCaseStr[j]) {
+          for (let k = 0; k < prePass.length; k++) {
+            if (prePass[k] != currentData) {
+              console.log("uppercase");
+              password += currentData;
+              console.log(password);
+
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    if (lowerCase !== undefined) {
+      for (let j = 0; j < lowerCaseStr.length; j++) {
+        if (currentData == lowerCaseStr[j]) {
+          for (let k = 0; k < prePass.length; k++) {
+            if (prePass[k] != currentData) {
+              console.log("lowercase");
+              password += currentData;
+              console.log(password);
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    if (number !== undefined) {
+      for (let j = 0; j < numberStr.length; j++) {
+        if (currentData == numberStr[j]) {
+          for (let k = 0; k < prePass.length; k++) {
+            if (prePass[k] != currentData) {
+              console.log("number");
+              password += currentData;
+              console.log(password);
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    if (symbols !== undefined) {
+      for (let j = 0; j < symbolsStr.length; j++) {
+        if (currentData == symbolsStr[j]) {
+          for (let k = 0; k < prePass.length; k++) {
+            if (prePass[k] != currentData) {
+              console.log("symbol");
+              password += currentData;
+              console.log(password);
+
+              break;
+            }
+          }
+        }
+      }
+    }
+
     start++;
   }
 
